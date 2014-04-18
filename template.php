632d17ee43f3ddb5e0e7f0bd339ed2701afcc14d
Zach Seifts <zach.seifts@gmail.com>
2014-04-18 11:14:10 -0400

    fixing the user_login, user_pass, and user_profile pages

--- template.php
+++ template.php
@@ -4,3 +4,41 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_theme()
+ */
+function hcbf_theme_theme() {
+  $themes = array();
+
+  $themes['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'hcbf_theme') . '/templates/system',
+    'template' => 'user-login',
+  );
+
+  $themes['user_pass'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'hcbf_theme') . '/templates/system',
+    'template' => 'user-pass',
+  );
+
+  return $themes;
+}
+
+/**
+ * Implements hook_page_alter()
+ */
+function hcbf_theme_page_alter(&$page) {
+  dpm($page);
+}
+
+/**
+ * Implements hook_form_FORM_ID_alter()
+ *
+ * Overrides the session_limit's session_limit_user_settings form.
+ */
+function hcbf_theme_form_session_limit_user_settings_alter(&$form, &$form_state, $form_id) {
+  $form['#prefix'] = '<div id="session_limit-wrapper" class="wrapper"><div class="container"><div class="row"><div class="col-xs-12">';
+  $form['#suffix'] = '</div></div></div></div>';
+}
---
+++ templates/system/user-pass.tpl.php
@@ -0,0 +1,12 @@
+<div class="user-pass-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8">
+        <?php print drupal_render_children($form) ?>
+      </div>
+      <div class="col-xs-12 col-sm-4">
+        <p>Maecenas convallis sapien ac dui dignissim imperdiet. Nullam eget porta nunc, sit amet tempus massa. Vestibulum non velit sit amet elit porta venenatis. Maecenas ut rhoncus mauris. Vivamus dapibus mauris eu scelerisque posuere. Duis et nunc ut nisl eleifend fringilla quis a tortor. Ut in dictum magna, non condimentum nunc.</p>
+      </div>
+    </div>
+  </div>
+</div>
