50d85dea30e595ee50505bbf13b0b70f07719992
Zach Seifts <zach.seifts@gmail.com>
2014-04-24 11:04:48 -0400

    Working more on the theme

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -73,46 +73,6 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
-</header>
-
 <div id="meta-wrapper">
   <div class="container">
     <div class="row">
@@ -142,8 +102,28 @@
 
 <?php print render($page['content']); ?>
 
-<div id="footer-wrapper">
-  <footer class="footer container">
-    <?php print render($page['footer']); ?>
-  </footer>
-</div>
+<footer id="footer-wrapper" class="inverse-wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <p>&copy; High Country Beer Fest [YYYY]</p>
+        <p>Footer 1</p>
+      </div>
+      <div class="col-xs-12 col-sm-2">
+        <p>Footer 2</p>
+      </div>
+      <div class="col-xs-12 col-sm-2">
+        <p>Footer 3</p>
+      </div>
+      <div class="col-xs-12 col-sm-2">
+        <h1>Follow us</h1>
+        <ul>
+          <li>Twitter</li>
+          <li>Facebook</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
--- templates/system/user-login.tpl.php
+++ templates/system/user-login.tpl.php
@@ -5,7 +5,8 @@
         <?php print drupal_render_children($form) ?>
       </div>
       <div class="col-xs-12 col-sm-4">
-        <p>Maecenas convallis sapien ac dui dignissim imperdiet. Nullam eget porta nunc, sit amet tempus massa. Vestibulum non velit sit amet elit porta venenatis. Maecenas ut rhoncus mauris. Vivamus dapibus mauris eu scelerisque posuere. Duis et nunc ut nisl eleifend fringilla quis a tortor. Ut in dictum magna, non condimentum nunc.</p>
+        <h1>You must login to access this content</h1>
+        <p>To view this content you need to log in first, if you do not have an account you should contact Zach.</p>
       </div>
     </div>
   </div>
