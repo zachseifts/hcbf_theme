238e82e03a3f270683d4278e7a4d7c152f1cb68b
Zach Seifts <zach.seifts@gmail.com>
2014-07-14 10:22:26 -0400

    Removing ipsum from the password reset page

--- templates/system/user-pass.tpl.php
+++ templates/system/user-pass.tpl.php
@@ -5,7 +5,7 @@
         <?php print drupal_render_children($form) ?>
       </div>
       <div class="col-xs-12 col-sm-4">
-        <p>Maecenas convallis sapien ac dui dignissim imperdiet. Nullam eget porta nunc, sit amet tempus massa. Vestibulum non velit sit amet elit porta venenatis. Maecenas ut rhoncus mauris. Vivamus dapibus mauris eu scelerisque posuere. Duis et nunc ut nisl eleifend fringilla quis a tortor. Ut in dictum magna, non condimentum nunc.</p>
+        <p>Use this form to reset your password.</p>
       </div>
     </div>
   </div>
