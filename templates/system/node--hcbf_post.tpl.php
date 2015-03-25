c135b85051758ea731d81a6648ff9fcb9e45b323
Zach Seifts <zach.seifts@gmail.com>
2015-03-25 11:05:41 -0400

    Fixing buy tickets block

--- templates/system/node--hcbf_post.tpl.php
+++ templates/system/node--hcbf_post.tpl.php
@@ -115,29 +115,7 @@
       </div>
     </div>
     <div class="col-xs-12 col-sm-6 col-lg-4">
-      <div class="well">
-        <div class="text-center">
-          <i class="icon fa fa-ticket fa-5x"></i>
-        </div>
-        <div class="page-header">
-          <h2 class="text-center">Buy Tickets</h2>
-        </div>
-        <p>Tickets are on sale now!</p>
-        <div class="table-responsive">
-          <table class="table ticket-table">
-            <tbody>
-              <tr>
-                <td>General Admission</td>
-                <td><a class="btn btn-success btn-sm" href="http://www.etix.com/ticket/online/performanceSearch.jsp?performance_id=1820947">$40.00</a></td>
-              </tr>
-              <tr>
-                <td>Designated Driver</td>
-                <td><a class="btn btn-success btn-sm" href="http://www.etix.com/ticket/online/performanceSearch.jsp?performance_id=1821065">$10.00</a></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <?php print theme('hcbf_breweries_on_sale', array('classes' => 'well')); ?>
     </div>
   </div>
 </div>
